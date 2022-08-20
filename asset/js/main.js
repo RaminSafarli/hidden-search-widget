80c0a18bc480693ee3e9cedebd89a26fa6286abe
RaminSafarli <ramin.seferli.kb@gmail.com>
2022-08-20 21:59:15 +0400

    simple search effect

---
+++ asset/js/main.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".btn");
+const input = document.querySelector(".input");
+const container = document.querySelector(".container");
+
+button.addEventListener("click", (e) => {
+  container.classList.toggle("active");
+});
